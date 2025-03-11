8871cfcea22118501cb4ef6999c4349c877509d1
JacinthV <joy237@gmail.com>
2025-03-12 02:42:44 +0530

    added env variables

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const databaseConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'gondola.proxy.rlwy.net',
-  port: 46797,
-  username: 'postgres',
-  password: 'owYAsYigpchTBWvoCRaXTOvfzwbboFfO',
-  database: 'railway',
+  type: process.env.DB_TYPE as any,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: false, // Disable automatic schema synchronization
   logging: true,
