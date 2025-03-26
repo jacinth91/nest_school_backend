43de86c605c2e7064e11648df235ffa38a64da83
JacinthV <joy237@gmail.com>
2025-03-26 13:39:42 +0530

    student fix

--- src/students/entities/student.entity.ts
+++ src/students/entities/student.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, BeforeInsert, BeforeUpdate } from 'typeorm';
 
 @Entity('students')
 export class Student {
@@ -14,6 +14,23 @@ export class Student {
 
   @Column({ name: 'student_name' })
   studentName: string;
+
+
+  @Column({ name: 'student_type' })
+  studentType
+
+  @Column({ name: 'admission_year' })
+  admissionYear: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  setStudentType() {
+    const currentYear = new Date().getFullYear();
+    this.studentType = this.admissionYear.includes(currentYear.toString()) ? 'New' : 'Existing';
+  }
+
+  @Column({ name: 'boarding' })
+  boardingStatus: string;
 
   // @Column({ name: 'father_name' })
   // fatherName: string;
